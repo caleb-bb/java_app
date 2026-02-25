7476d5645151ffeaaaf39546815f88e87c9d0431
admin <admin@admins-MacBook-Pro.local>
2026-02-25 16:21:29 -0500

    All tests pass

---
+++ src/main/java/com/caleb/chase/customerapi/CustomerServiceImpl.java
@@ -0,0 +1,14 @@
+package com.caleb.chase.customerapi;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+// The @Service annotation tells Spring "this is a bean, so register it in the context"
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public List<Customer> findAll() {
+        return List.of();
+    }
+}
